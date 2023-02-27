f7921b7a469c5d799b31b96b335de2b44be33990
Cral-Cactus <john.j.smithg@gmail.com>
2023-02-27 18:42:29 +0200

    Add input number

--- AddNumberToArray/Program.cs
+++ AddNumberToArray/Program.cs
@@ -8,6 +8,15 @@ namespace AddNumberToArray
         static void Main()
         {
             int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            int[] newArray = new int[array.Length + 1];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[array.Length] = number;
+            array = newArray;
 
             Array.Sort(array);
             
